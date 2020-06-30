1eca4a2beda4358f326e430341a7502316d8509a
super <ext.xushoujun@jd.com>
2020-06-30 12:40:09 +0800

    add gitignore

---
+++ OpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL
+//
+//  Created by super on 2020/6/30.
+//  Copyright © 2020 super. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
